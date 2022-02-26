15b4b31df5c7a2b4b28fc556364f8eb2d79c95bd
Hecksmosis <pgruiz727@gmail.com>
2022-02-26 12:51:30 +0100

    aaa

--- bootsector.asm
+++ bootsector.asm
@@ -1,28 +1,30 @@
-; A boot sector that enters 32 - bit protected mode.
 [org 0x7c00]
 
-mov bp, 0x9000 ; Set the stack.
+mov bp, 0x9000
 mov sp, bp
 mov bx, MSG_REAL_MODE
 call print_string
-call switch_to_pm ; Note that we never return from here.
+call switch_to_pm
 jmp $
-%include "../print/print_string.asm"
-%include "gdt.asm"
+
+%include "print_string.asm"
 %include "print_string_pm.asm"
+%include "gdt.asm"
 %include "switch_to_pm.asm"
 
 [bits 32]
-
-; This is where we arrive after switching to and initialising protected mode.
 BEGIN_PM:
-    mov ebx, MSG_PROT_MODE
-    call print_string_pm ; Use our 32 - bit print routine.
-    jmp $ ; Hang.
+	mov ebx, MSG_PROT_MODE
+	call print_string_pm
+	jmp $
 
-; Global variables
-MSG_REAL_MODE db "Started in 16 - bit Real Mode", 0
-MSG_PROT_MODE db "Successfully landed in 32 - bit Protected Mode", 0
-; Bootsector padding
+
+MSG_REAL_MODE:
+	db "Started in 16- bit Real Mode", 0
+
+MSG_PROT_MODE:
+	db "Successfully landed in 32- bit Protected Mode", 0
+
+
 times 510-($-$$) db 0
 dw 0xaa55
